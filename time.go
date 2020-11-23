c13d523930ba6b98e77c1614d892699859657671
程金盛 <jinsheng.cheng@handeson.com>
2020-11-23 11:55:36 +0800

    edit time

--- time.go
+++ time.go
@@ -26,6 +26,17 @@ func Time(timezone ...*time.Location) int64 {
 	return time.Now().In(loc).Unix()
 }
 
+//format="20060102"  gosupport.TimeNow2Format("20060102") fmt.Println(gosupport.TimeNow2Format("2006.01.02 15:04:05"))
+func TimeNow2Format(format string, timezone ...*time.Location) string {
+	var loc *time.Location
+	if len(timezone) == 0 {
+		loc = GetShanghaiTimezone()
+	} else {
+		loc = timezone[0]
+	}
+	return time.Now().In(loc).Format(format)
+}
+
 //unixTime,_ := gosupport.Strtotime(gosupport.TIME_FORMAT,"2021-01-02 02:36:43")
 func Strtotime(format, strtime string,timezone ...*time.Location) (int64, error) {
 	var loc *time.Location
@@ -118,43 +129,80 @@ func TimePtr2Str2(t time.Time) string {
 }
 
 //当前时间转年月日格式，返回示例：20191128
-func TimeNow2String() string {
-	t := time.Now()
+func TimeNow2String(timezone ...*time.Location) string {
+	var loc *time.Location
+	if len(timezone) == 0 {
+		loc = GetShanghaiTimezone()
+	} else {
+		loc = timezone[0]
+	}
+	t := time.Now().In(loc)
 	timeNow := t.Format("20060102")
 	return timeNow
 }
 
 //当前时间转日格式，返回示例：28
-func TimeNow2Day() int {
-	t := time.Now()
+func TimeNow2Day(timezone ...*time.Location) int {
+	var loc *time.Location
+	if len(timezone) == 0 {
+		loc = GetShanghaiTimezone()
+	} else {
+		loc = timezone[0]
+	}
+	t := time.Now().In(loc)
 	timeNow := t.Format("02")
 	return Str2Int(timeNow)
 }
 
 //当前时间转月格式，返回示例：11
-func TimeNow2Month() int {
-	t := time.Now()
+func TimeNow2Month(timezone ...*time.Location) int {
+	var loc *time.Location
+	if len(timezone) == 0 {
+		loc = GetShanghaiTimezone()
+	} else {
+		loc = timezone[0]
+	}
+	t := time.Now().In(loc)
 	timeNow := t.Format("01")
 	return Str2Int(timeNow)
 }
 
 //当前时间转年月格式，返回示例：201911
-func TimeNow2YearMonth() int {
-	t := time.Now()
+func TimeNow2YearMonth(timezone ...*time.Location) int {
+	var loc *time.Location
+	if len(timezone) == 0 {
+		loc = GetShanghaiTimezone()
+	} else {
+		loc = timezone[0]
+	}
+	t := time.Now().In(loc)
 	timeNow := t.Format("200601")
 	return Str2Int(timeNow)
 }
 
 //当前时间返回年月日
-func TimeNow2YMD() (int, int, int)  {
-	year, month, day := time.Now().Date()
+func TimeNow2YMD(timezone ...*time.Location) (int, int, int)  {
+	var loc *time.Location
+	if len(timezone) == 0 {
+		loc = GetShanghaiTimezone()
+	} else {
+		loc = timezone[0]
+	}
+	year, month, day := time.Now().In(loc).Date()
 	return year, int(month), day
 }
 
 //时间戳转日期时间格式,调用示例： gosupport.timestamp2DateTime(1569152644, 7)
-func timestamp2DateTime(timestamp int, way int) string  {
+func timestamp2DateTime(timestamp int, way int,timezone ...*time.Location) string  {
 	var ret string
-	timeObj := time.Unix(int64(timestamp), 0) //将时间戳转为时间格式
+	var loc *time.Location
+	if len(timezone) == 0 {
+		loc = GetShanghaiTimezone()
+	} else {
+		loc = timezone[0]
+	}
+
+	timeObj := time.Unix(int64(timestamp), 0).In(loc) //将时间戳转为时间格式
 	year := timeObj.Year()     //年
 	month := timeObj.Month()   //月
 	day := timeObj.Day()       //日
@@ -230,9 +278,15 @@ func DateT(format string, t time.Time) string {
 在今年显示为：N月N日 N点N分（如：02月03日 09:33）
 今年以前显示为：N年N月N日 N点N分（如：2020年09月18日 15:59）
 */
-func SubTimeStr(t2 time.Time) string {
+func SubTimeStr(t2 time.Time, timezone ...*time.Location) string {
 	var ret string
-	t1 := time.Now()
+	var loc *time.Location
+	if len(timezone) == 0 {
+		loc = GetShanghaiTimezone()
+	} else {
+		loc = timezone[0]
+	}
+	t1 := time.Now().In(loc)
 	t1UnixTime := t1.Unix() //当前时间
 	t2UnixTime := t2.Unix()
 	if t1UnixTime<t2UnixTime {
@@ -260,9 +314,15 @@ func SubTimeStr(t2 time.Time) string {
 /**
  * 已运行时长: d天h小时m分钟s秒
  */
-func AlreadyTimeStr(t2 time.Time) string {
+func AlreadyTimeStr(t2 time.Time, timezone ...*time.Location) string {
 	var ret string
-	t1 := time.Now()
+	var loc *time.Location
+	if len(timezone) == 0 {
+		loc = GetShanghaiTimezone()
+	} else {
+		loc = timezone[0]
+	}
+	t1 := time.Now().In(loc)
 	t1UnixTime := t1.Unix() //当前时间秒
 	t2UnixTime := t2.Unix()
 	if t1UnixTime<t2UnixTime {
@@ -296,3 +356,33 @@ func SubDays(t1, t2 time.Time) (day int) {
 	day = int(t1.Sub(t2).Hours() / 24)
 	return
 }
+
+
+//今天开始时间和结束时间
+func TodayStartEndTime(timezone ...*time.Location) (time.Time, time.Time) {
+	var loc *time.Location
+	if len(timezone) == 0 {
+		loc = GetShanghaiTimezone()
+	} else {
+		loc = timezone[0]
+	}
+	today := time.Now().In(loc).Format("2006-01-02")
+	start, _ := time.ParseInLocation("2006-01-02 15:04:05", today+" 00:00:00", loc)
+	end, _ := time.ParseInLocation("2006-01-02 15:04:05", today+" 23:59:59", loc)
+	return start, end
+}
+
+//某天的开始时间和结束时间
+func DayStartEndTime(t time.Time, timezone ...*time.Location) (time.Time, time.Time) {
+	var loc *time.Location
+	if len(timezone) == 0 {
+		loc = GetShanghaiTimezone()
+	} else {
+		loc = timezone[0]
+	}
+	today := t.In(loc).Format("2006-01-02")
+	start, _ := time.ParseInLocation("2006-01-02 15:04:05", today+" 00:00:00", loc)
+	end, _ := time.ParseInLocation("2006-01-02 15:04:05", today+" 23:59:59", loc)
+	return start, end
+}
+
--- time_test.go
+++ time_test.go
@@ -16,12 +16,13 @@ func TestTimeFormat2Date(t *testing.T) {
 	fmt.Println(TimePtr2Str(TimeNowPtr()))
 	fmt.Println(TimePtr2Str2(time.Now()))
 
-	fmt.Println(TimeNow2String())
+	fmt.Println("TimeNow2String: ",TimeNow2String())
 
-	fmt.Println(TimeNow2YearMonth())
-	fmt.Println(TimeNow2Month())
-	fmt.Println(TimeNow2Day())
-	fmt.Println(TimeNow2YMD())
+	fmt.Println("TimeNow2YearMonth:",TimeNow2YearMonth())
+	fmt.Println("TimeNow2Month:",TimeNow2Month())
+	fmt.Println("TimeNow2Day:",TimeNow2Day())
+	y,m,d := TimeNow2YMD()
+	fmt.Println("TimeNow2YMD: ", y,m,d)
 
 	//星期
 	fmt.Println(time.Now().Weekday().String()) //Thursday
@@ -30,8 +31,8 @@ func TestTimeFormat2Date(t *testing.T) {
 	//当前时间戳 1574907260
 	fmt.Println(time.Now().Unix())
 
-	fmt.Println(timestamp2DateTime(1569152644, 7))
-
+	fmt.Println("timestamp2DateTime:", timestamp2DateTime(1569152644, 7))
+	fmt.Println("TimeNow2Format:",TimeNow2Format("2006.01.02 15:04:05"))
 }
 
 func TestDateT(t *testing.T) {
@@ -46,7 +47,19 @@ func TestDateT(t *testing.T) {
 func TestSubDays(t *testing.T) {
 	//相差天数
 	hour,_ := time.ParseDuration("-25h") //25小时前
-	t2 := time.Now().Add(hour)
-	fmt.Println(SubDays(time.Now(), t2))
+	t2 := time.Now().In(GetShanghaiTimezone()).Add(hour)
+	fmt.Println(SubDays(time.Now().In(GetShanghaiTimezone()), t2))
+
+}
+
+func TestTodayStartEndTime(t *testing.T) {
+	s, e := TodayStartEndTime(GetShanghaiTimezone())
+	fmt.Println(s.Format(TIME_FORMAT), e.Format(TIME_FORMAT))
+	fmt.Println(s.Unix(), e.Unix())
+
+	dayTime, _ := time.ParseInLocation("2006-01-02", "2019-11-09", GetShanghaiTimezone())
+	s2,e2 := DayStartEndTime(dayTime)
+	fmt.Println(s2.Format(TIME_FORMAT), e2.Format(TIME_FORMAT))
+	fmt.Println(s2.Unix(), e2.Unix())
 
 }
