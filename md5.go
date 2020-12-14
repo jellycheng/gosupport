d0ab78b43e991cb53a51604a8618e8b800827bdb
程金盛 <jinsheng.cheng@handeson.com>
2020-12-14 11:44:36 +0800

    edit md5

--- md5.go
+++ md5.go
@@ -2,7 +2,9 @@ package gosupport
 
 import (
 	"crypto/md5"
+	"encoding/hex"
 	"fmt"
+	"io"
 )
 
 //md5加密
@@ -11,6 +13,23 @@ func Md5V1(str string) string {
 	h.Write([]byte(str))  //字符串转byte数组,并写入内容
 	has := h.Sum([]byte(""))  //返回[]byte
 	md5str := fmt.Sprintf("%x", has)
-	return string(md5str)
+	return md5str
 }
 
+func Md5V2(str string) string  {
+	h := md5.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func Md5V3(str string) string {
+	w := md5.New()
+	io.WriteString(w, str)
+	md5str := fmt.Sprintf("%x", w.Sum(nil))
+	return md5str
+}
+
+func Md5V4(s string) string {
+	m := md5.Sum([]byte(s))
+	return hex.EncodeToString(m[:])
+}
