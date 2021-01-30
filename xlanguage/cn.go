698f7c11abcc49fda58a4aebab742f74b2a8b47d
程金盛 <jinsheng.cheng@handeson.com>
2021-01-30 17:51:26 +0800

    add language

---
+++ xlanguage/cn.go
@@ -0,0 +1,38 @@
+package xlanguage
+
+var CN = LangSet{
+	"username":      "用户名",
+	"nickname":  "昵称",
+	"role":      "角色",
+	"createdat": "创建时间",
+	"updatedat": "更新时间",
+	"path":      "路径",
+	"submit":    "提交",
+	"filter":    "筛选",
+
+	"new":             "新建",
+	"export":          "导出",
+	"action":          "操作",
+	"toggle_dropdown": "下拉",
+	"delete":          "删除",
+	"refresh":         "刷新",
+	"back":            "返回",
+	"reset":           "重置",
+	"save":            "保存",
+	"edit":            "编辑",
+	"expand":          "展开",
+	"collapse":        "折叠",
+	"online":          "在线",
+	"setting":         "设置",
+	"sign.out":        "退出",
+	"sign.in":         "登录",
+	"sign.up":         "注册",
+	"login":           "登录",
+	"register":        "注册",
+	"home":            "首页",
+	"all":             "全部",
+	"more":            "更多",
+	"browse":          "打开",
+	"remove":          "移除",
+}
+
---
+++ xlanguage/language.go
@@ -0,0 +1,26 @@
+package xlanguage
+
+//单个语言的key-vaule集合
+type LangSet map[string]string
+
+func (l LangSet) Add(key, value string) {
+	l[key] = value
+}
+
+func (l LangSet) Get(key string) string {
+	if ret, ok := l[key];ok {
+		return ret
+	}
+	return ""
+}
+
+func (l LangSet) Combine(set LangSet) LangSet {
+	for k, s := range set {
+		l[k] = s
+	}
+	return l
+}
+
+type LangMap map[string]LangSet
+
+
