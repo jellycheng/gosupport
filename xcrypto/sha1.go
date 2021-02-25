d55e02559ed61098da214ba4842a669314c2bc8f
程金盛 <jinsheng.cheng@handeson.com>
2021-02-25 18:09:34 +0800

    add sha256

--- xcrypto/sha1.go
+++ xcrypto/sha1.go
@@ -2,6 +2,7 @@ package xcrypto
 
 import (
 	"crypto/sha1"
+	"crypto/sha256"
 	"fmt"
 	"io"
 )
@@ -12,3 +13,14 @@ func Sha1V1(str string) string  {
 	return fmt.Sprintf("%x", h.Sum(nil))
 }
 
+func Sha256V1(str string) string  {
+	h := sha256.New()
+	_, _ = io.WriteString(h, str)
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func Sha256ByByteV1(by []byte) string  {
+	h := sha256.New()
+	h.Write(by)
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
