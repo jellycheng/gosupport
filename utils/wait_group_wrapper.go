d2aedaf9a06667e372732de8f897d4ad21900b01
jellycheng <42282367@qq.com>
2021-06-30 07:02:01 +0800

    Create wait_group_wrapper.go

---
+++ utils/wait_group_wrapper.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"sync"
+)
+
+type WaitGroupWrapper struct {
+	sync.WaitGroup
+}
+
+func (w *WaitGroupWrapper) Wrap(callback func()) {
+	w.Add(1)
+	go func() {
+		callback()
+		w.Done()
+	}()
+}
+
