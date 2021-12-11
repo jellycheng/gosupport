ebe93cf1923494391084b1b79b96aa89d71e53a9
程金盛 <jinsheng.cheng@handeson.com>
2021-12-11 22:46:37 +0800

    edit net

--- curl/index.go
+++ curl/index.go
@@ -1,68 +1,95 @@
 package curl
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"io/ioutil"
 	"net/http"
-	neturl "net/url"
+	"net/url"
 	"strings"
 )
 
-/*
-  发起get请求
- */
-func HttpGet(url string) (string, error) {
+// HttpGet 发起get请求
+func HttpGet(urlStr string) (string, error) {
 	var content string
-	resp, err := http.Get(url)
+	resp, err := http.Get(urlStr)
 	if err!=nil {
 		return "", err
 	}
-	defer resp.Body.Close()  //关闭资源
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(resp.Body)
 	body, _ := ioutil.ReadAll(resp.Body)
 	content = string(body)
 
 	return content, nil
 }
 
-/*
-  发起表单post请求
-  param := "name=李四&age=19"
- */
-func HttpPost(url string, param string) (string, error)  {
-	resp, err := http.Post(url, CONTENT_TYPE_FORM, strings.NewReader(param))
+// HttpPost 发起表单post请求
+// param := "name=李四&age=19"
+func HttpPost(urlStr string, param string) (string, error)  {
+	resp, err := http.Post(urlStr, CONTENT_TYPE_FORM, strings.NewReader(param))
 	if err!=nil {
 		return "", err
 	}
-	defer resp.Body.Close()
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(resp.Body)
 	body, _ := ioutil.ReadAll(resp.Body)
 	content := string(body)
 
 	return content, nil
 }
 
-func HttpPostJson(url string, param string) (string, error)  {
-	resp, err := http.Post(url, CONTENT_TYPE_JSON, strings.NewReader(param))
+func HttpPostJson(urlStr string, param string) (string, error)  {
+	resp, err := http.Post(urlStr, CONTENT_TYPE_JSON, strings.NewReader(param))
 	if err!=nil {
 		return "", err
 	}
-	defer resp.Body.Close()
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(resp.Body)
 	body, _ := ioutil.ReadAll(resp.Body)
 	content := string(body)
 
 	return content, nil
 }
 
-/*
-  param := url.Values{"参数名1": {"值1"}, "参数名2": {"值1","值N"}}
- */
-func HttpPostForm(url string, param neturl.Values) (string, error)  {
-	resp, err := http.PostForm(url, param)
+// HttpPostForm param := url.Values{"参数名1": {"值1"}, "参数名2": {"值1","值N"}}
+func HttpPostForm(urlStr string, param url.Values) (string, error)  {
+	resp, err := http.PostForm(urlStr, param)
 	if err!=nil {
 		return "", err
 	}
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	content := string(body)
-
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(resp.Body)
+	var body, _ = ioutil.ReadAll(resp.Body)
+	var content = string(body)
 	return content, nil
 }
 
+func ReadBodyContent(httpResp *http.Response) (string, error) {
+	var body, err = ReadBodyContent2Byte(httpResp)
+	return string(body), err
+}
+
+func ReadBodyContent2Byte(httpResp *http.Response) ([]byte, error) {
+	var body, err = ioutil.ReadAll(httpResp.Body)
+	_ = httpResp.Body.Close()
+	if err != nil {
+		return body, err
+	}
+	httpResp.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+	return body, err
+}
+
+func UnMarshalResponse(httpResp *http.Response, resp interface{}) error {
+	var body, err = ReadBodyContent2Byte(httpResp)
+	err = json.Unmarshal(body, resp)
+	if err != nil {
+		return err
+	}
+	return nil
+}
