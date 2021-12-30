45034aa5cb1ba8bcae9fe79eca5a5ad07f822f9c
程金盛 <jinsheng.cheng@handeson.com>
2021-12-30 14:56:46 +0800

    fixed price

--- price.go
+++ price.go
@@ -11,21 +11,26 @@ import (
 	s1 := gosupport.Fen2yuan(int64(price), true)
  */
 func Fen2yuan(price int64, isTrimZero bool) string {
+	mathSymbol := ""
+	if price <0 {
+		mathSymbol = "-"
+		price = price * -1
+	}
 	tmpPrice01 := price/100
 	//tmpPrice02 := price - tmpPrice01 * 100
 	tmpPrice02 := price%100
 	formatPrice := ""
 	if tmpPrice02<10 && tmpPrice02 >0 {
-		formatPrice = fmt.Sprintf("%d.0%d", tmpPrice01, tmpPrice02)
+		formatPrice = fmt.Sprintf("%s%d.0%d", mathSymbol,tmpPrice01, tmpPrice02)
 	} else if tmpPrice02 == 0 {
 		if isTrimZero {
 			isTrimZero = false
-			formatPrice = fmt.Sprintf("%d", tmpPrice01)
+			formatPrice = fmt.Sprintf("%s%d", mathSymbol,tmpPrice01)
 		} else {
-			formatPrice = fmt.Sprintf("%d.00", tmpPrice01)
+			formatPrice = fmt.Sprintf("%s%d.00", mathSymbol,tmpPrice01)
 		}
 	} else {
-		formatPrice = fmt.Sprintf("%d.%d", tmpPrice01, tmpPrice02)
+		formatPrice = fmt.Sprintf("%s%d.%d", mathSymbol,tmpPrice01, tmpPrice02)
 	}
 
 	if isTrimZero {
