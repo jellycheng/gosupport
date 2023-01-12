de94650240149abe31d81c2b73b115d2ef71ffb5
程金盛 <jinsheng.cheng@handeson.com>
2023-01-12 19:15:44 +0800

    add api dto

--- dto.go
+++ dto.go
@@ -1,5 +1,7 @@
 package gosupport
 
+import "encoding/json"
+
 type ListRespDto struct {
 	List interface{} `json:"list"`
 }
@@ -10,4 +12,11 @@ type PagerRespDto struct {
 	PageSize int64       `json:"page_size"`
 	Total    int64       `json:"total"`
 	List     interface{} `json:"list"`
+}
+
+type ApiUnRespDto struct {
+	Code    int             `json:"code"`
+	Msg     string          `json:"msg"`
+	Data    json.RawMessage `json:"data"`
+	TraceId string          `json:"trace_id,omitempty"`
 }
