8f37cee34faf5a17b7e363b7abfe98d7082553ea
程金盛 <42282367@qq.com>
2023-11-23 20:59:52 +0800

    add maxwell dto

--- dto.go
+++ dto.go
@@ -20,3 +20,19 @@ type ApiUnRespDto struct {
 	Data    json.RawMessage `json:"data"`
 	TraceId string          `json:"trace_id,omitempty"`
 }
+
+type MaxwellDto struct {
+	Database          string          `json:"database"` // 库名
+	Table             string          `json:"table"`    // 表名
+	Type              string          `json:"type"`     // 类型
+	Ts                int64           `json:"ts"`       //秒级别的时间戳
+	Xid               int64           `json:"xid"`      //事务id
+	Commit            bool            `json:"commit"`
+	Position          string          `json:"position,omitempty"`
+	ServerID          int64           `json:"server_id,omitempty"`
+	ThreadID          int64           `json:"thread_id,omitempty"`
+	PrimaryKey        []interface{}   `json:"primary_key,omitempty"`
+	PrimaryKeyColumns []string        `json:"primary_key_columns,omitempty"`
+	Data              json.RawMessage `json:"data"`
+	Old               json.RawMessage `json:"old,omitempty"`
+}
